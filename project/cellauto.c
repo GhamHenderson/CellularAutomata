0f768b9f9d70797a01b99c34937b0c28f227f687
Graham Henderson <62728935+GhamHenderson@users.noreply.github.com>
2021-12-03 16:13:41 +0000

    Updated Version

--- project/cellauto.c
+++ project/cellauto.c
@@ -14,34 +14,43 @@ int main(){
   int cols = SIZE;
   int cells[cols][rows];
   int futurecells[cols][rows];
-
-  printf("\nGeneration 1 \n\n");
+   printf("\n cells \n\n");
 
   // Print first generation array and assign values to both arrays.
-  for(i = 0; i < rows; i++){
-    for(j = 0; j < cols; j++){
+  for(i = 0; i < rows; i++)
+  {
+    for(j = 0; j < cols; j++)
+    {
       cells[i][j] = states[0];
-      cells[4][7] = states[2];
+      cells[0][1] = states[2];
+      cells[0][8] = states[2];
+      cells[8][1] = states[2];
+      cells[8][8] = states[2];
       printf("%d ", cells[i][j]);
     }  
-  printf("\n");
+    printf("\n");
   }
-  printf("\nGeneration 2\n\n");
+  printf("\n futureCells \n\n");
 
   // Print and assign values to future array.
-    for(i = 0; i < rows; i++){
-    for(j = 0; j < cols; j++){
+    for(i = 0; i < rows; i++)
+    {
+      for(j = 0; j < cols; j++)
+      {
       
-      futurecells[i][j] = states[0];
-      futurecells[4][7] = states[2];
+      futurecells[i][j] = cells[i][j];
       printf("%d ", futurecells[i][j]);
     }  
     printf("\n");
   }
   printf("\n\n\n");
  
-  int counter = 0;
+  int counter = 1;
   unsigned int suscells = 0;
+  unsigned int expcells = 0;
+  unsigned int infectedcells = 0;
+  unsigned int recoveredcells = 0;
+  unsigned int deadcells = 0;
   
   // Check if a cell is sus
   for(i = 0; i < rows; i++)
@@ -49,15 +58,217 @@ int main(){
     for(j = 0; j < cols; j++)
     {
       if(cells[i][j] == states[0])
-	{
+      {
         suscells++;
-	//check if cells around sus cell are infected and if so how many, and make current cell exposed
+	
+      }
 
+      	//check if cells around sus cell are infected and if so make current cell exposed
+        //Top Left Corner
+       if(cells[0][0] == states[0])
+       {
+	  if(cells[i][j+1] == states[2])
+	  {
+	    futurecells[0][0] = states[1];
+	  }
+	  else if(cells[i+1][j] == states[2])
+	  {
+	    futurecells[0][0] = states[1];
+	  }
+	   else if(cells[i+1][j+1] == states[2])
+	  {
+	    futurecells[0][0] = states[1];
+	  }
+       }
+
+	//Top right corner
+       if(cells[0][SIZE-1] == states[0])
+       {
+	  if(cells[i][j-1] == states[2])
+	  {
+	    futurecells[0][SIZE-1] = states[1];
+	  }
+	  else if(cells[i+1][j-1] == states[2])
+	  {
+	    futurecells[0][SIZE-1] = states[1];
+	  }
+	   else if(cells[i+1][j] == states[2])
+	  {
+	    futurecells[0][SIZE-1] = states[1];
+	  }
+       }
+
+	//Bottom Left Corner
+       if(cells[SIZE-1][0] == states[0])
+       {
+	  if(cells[i-1][j] == states[2])
+	  {
+	    futurecells[SIZE-1][0] = states[1];
+	  }
+	  else if(cells[i+1][j+1] == states[2])
+	  {
+	     futurecells[SIZE-1][0] = states[1];
+	  }
+	   else if(cells[i][j+1] == states[2])
+	  {
+	     futurecells[SIZE-1][0] = states[1];
+	  }
+       }
+
+       //Bottom Right Corner
+       if(cells[SIZE-1][SIZE-1] == states[0])
+       {
+	  if(cells[i-1][j] == states[2])
+	  {
+	    futurecells[SIZE-1][SIZE-1] = states[1];
+	  }
+	  else if(cells[i-1][j-1] == states[2])
+	  {
+	     futurecells[SIZE-1][SIZE-1] = states[1];
+	  }
+	   else if(cells[i][j-1] == states[2])
+	  {
+	     futurecells[SIZE-1][SIZE-1] = states[1];
+	  }
+       }
+    }
+  }
+
+  //check the first row
+  for(i = 0; i < rows-1; i++)
+  {
+    for(j = 1; j < cols-1; j++)
+    {
+      if(cells[0][j] == states[0])
+      {
+	if(cells[0][j-1] == states[2])
+	{
+	  futurecells[0][j] = states[1];
+	}
+	else if(cells[1][j-1] == states[2])
+	{
+	  futurecells[0][j] = states[1];
+	}
+	else if(cells[1][j] == states[2])
+	{
+	  futurecells[0][j] = states[1];
+	}
+	else if(cells[1][j+1] == states[2])
+	{
+	  futurecells[0][j] = states[1];
+	}
+	else if(cells[0][j+1] == states[2])
+	{
+	  futurecells[0][j] = states[1];
+	}
+      } 
+    }
+  }
+
+   //check the bottom row
+  for(i = SIZE-1; i < rows-1; i++)
+  {
+    for(j = 1; j < cols-1; j++)
+    {
+      if(cells[SIZE-1][j] == states[0])
+      {
+	if(cells[SIZE-1][j-1] == states[2])
+	{
+	  futurecells[SIZE-1][j] = states[1];
+	}
+	else if(cells[SIZE-1][j+1] == states[2])
+	{
+	  futurecells[SIZE-1][j] = states[1];
+	}
+	else if(cells[SIZE-2][j] == states[2])
+	{
+	  futurecells[SIZE-1][j] = states[1];
+	}
+	else if(cells[SIZE-2][j+1] == states[2])
+	{
+	  futurecells[SIZE-1][j] = states[1];
+	}
+	else if(cells[SIZE-2][j-1] == states[2])
+	{
+	  futurecells[SIZE-1][j] = states[1];
+	}
+      } 
+    }
+  }
+
+  
+
+  
+  
+   // Check if a cell is exposed
+  for(i = 0; i < rows; i++)
+  {
+    for(j = 0; j < cols; j++)
+    {
+      if(cells[i][j] == states[1])
+      {
+        expcells++;
+	//check if cells around exposed cell are infected and calculate chance of infection
+	//calculate chance of infection based on amount of infected neighbours and RNG between 0 and 1
       }
     }
   }
 
-  //calculate chance of infection based on amount of infected neighbours and RNG between 0 and 1
+   // Check if a cell is infected
+  for(i = 0; i < rows; i++)
+  {
+    for(j = 0; j < cols; j++)
+    {
+      if(cells[i][j] == states[2])
+      {
+        infectedcells++;
+        
+	//50% chance of death
+	//50% chance of recovery
+	//generate a random number between 0 and 1 and if greater than 0.5, make cell dead / if less than or equal to 0.5 the cell recovers and gains immunity 
+      }
+    }
+  }
+
+   // Check if a cell is recovered
+  for(i = 0; i < rows; i++)
+  {
+    for(j = 0; j < cols; j++)
+    {
+      if(cells[i][j] == states[3])
+      {
+	recoveredcells++;
+      }
+    }
+  }
+
+   // Check if a cell is dead
+  for(i = 0; i < rows; i++)
+  {
+    for(j = 0; j < cols; j++)
+    {
+      if(cells[i][j] == states[3])
+      {
+	deadcells++;
+      }
+    }
+  }
+
+  printf("\nGeneration: %d \n\n", counter);
+  counter++;
+  // Print first generation array and assign values to both arrays.
+  for(i = 0; i < rows; i++)
+  {
+    for(j = 0; j < cols; j++)
+    {
+      printf("%d ", futurecells[i][j]);
+    }  
+    printf("\n");
+  }
     
-  printf("\n\nSus Cells = %d \n\n", suscells);
+  printf("\nSus Cells = %d \n\n", suscells);
+  printf("\nExposed Cells = %d \n\n", expcells);
+  printf("\nInfected Cells = %d \n\n", infectedcells);
+  printf("\nRecovered Cells = %d \n\n", recoveredcells);
+  printf("\nDead Cells = %d \n\n", deadcells);
 } 
