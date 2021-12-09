d478d25bb429e262562cc5e9d489259168a82a1f
Graham Henderson <62728935+GhamHenderson@users.noreply.github.com>
2021-12-09 21:44:53 +0000

    update

---
+++ cmake-build-debug/CMakeFiles/CellularAutomata.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Graha/Documents/GitHub/CellularAutomata/project/cellautoMain.c" "C:/Users/Graha/Documents/GitHub/CellularAutomata/cmake-build-debug/CMakeFiles/CellularAutomata.dir/project/cellautoMain.c.obj"
+  "C:/Users/Graha/Documents/GitHub/CellularAutomata/project/cellautochild.c" "C:/Users/Graha/Documents/GitHub/CellularAutomata/cmake-build-debug/CMakeFiles/CellularAutomata.dir/project/cellautochild.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../project"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CellularAutomata.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CellularAutomata.dir/project/cellautoMain.c.obj"
+  "CMakeFiles/CellularAutomata.dir/project/cellautochild.c.obj"
+  "CellularAutomata.exe"
+  "CellularAutomata.exe.manifest"
+  "CellularAutomata.pdb"
+  "libCellularAutomata.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CellularAutomata.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
